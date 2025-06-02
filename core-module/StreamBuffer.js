fed7cacb338c0af135dbfd787f8ee61f6b3167af
unknown <raufurislam@gmail.com>
2025-06-02 14:26:48 +0600

    Stream and Buffer

--- core-module/StreamBuffer.js
+++ core-module/StreamBuffer.js
@@ -1,3 +1,4 @@
+// streamBuffer.js
 const fs = require("fs");
 
 // fs.readFile("./hello-world.txt", { encoding: "utf-8" }, (err, data) => {
@@ -13,3 +14,39 @@ const fs = require("fs");
 //   });
 //   console.log("Written Successfully");
 // });
+
+const readStream = fs.createReadStream("./hello-world.txt", {
+  encoding: "utf-8",
+});
+const writeStream = fs.createWriteStream("./hello.txt", { encoding: "utf-8" });
+
+readStream.on("data", (data) => {
+  console.log(data);
+
+  writeStream.write(data, (err) => {
+    if (err) {
+      throw Error("Error", err);
+    }
+  });
+});
+
+readStream.on("error", (err) => {
+  if (err) {
+    throw Error("Error", err);
+  }
+});
+
+writeStream.on("error", (err) => {
+  if (err) {
+    throw Error("Error", err);
+  }
+});
+
+readStream.on("end", () => {
+  console.log("Reading Ended");
+  writeStream.end();
+});
+
+writeStream.on("finish", () => {
+  console.log("Written Successfully");
+});
