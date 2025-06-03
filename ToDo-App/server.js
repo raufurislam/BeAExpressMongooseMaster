d499db407454a28fabe744303def56c166fcecdd
unknown <raufurislam@gmail.com>
2025-06-03 15:00:37 +0600

    Simple route handling

--- ToDo-App/server.js
+++ ToDo-App/server.js
@@ -1,10 +1,24 @@
+// Basic Todo app with nodejs
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log({ req, res });
-  res.end("Welcome to ToDo app server");
+  console.log(req.url, req.method);
+  // res.end("Welcome to ToDo app server");
+
+  if (req.url === "/todos" && req.method === "GET") {
+    res.end("All Todos here");
+  } else if (req.url === "/create-todo" && req.method === "POST") {
+    res.end("Todo created");
+  } else {
+    res.end("Route not found");
+  }
 });
 
 server.listen(5000, "127.0.0.1", () => {
   console.log("✅ Server listening to port 5000");
 });
+
+/**
+ * /todos - GET - All Todo
+ * /todos/create-todo - POST - Create Todo
+ */
