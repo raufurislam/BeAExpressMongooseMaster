2770179b0d6f352fed956400cd367877ca597235
Raufur Islam <raufurislam@gmail.com>
2025-06-09 01:01:04 +0600

    Parser request and response object

--- module-3/dist/app.js
+++ module-3/dist/app.js
@@ -4,8 +4,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
+const filePath = path_1.default.join(__dirname, "../db/todo.json");
+app.use(express_1.default.json());
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    console.log(req.url, req.method);
+    res.send("Welcome to Todos App");
+});
+app.get("/todos", (req, res) => {
+    const data = fs_1.default.readFileSync(filePath, { encoding: "utf-8" });
+    console.log(data);
+    res.json(data);
+});
+app.post("/todos/create-todo", (req, res) => {
+    const { title, body } = req.body;
+    console.log(title, body);
+    res.send("Creating todo");
 });
 exports.default = app;
+/**
+ * Basic file structure
+ * server - server handling like - starting, closing error handling of server. only related to server
+ * app file - routing handle, middleware, route related error
+ * app folder - app business logic handling like CRUD and database related work
+ */
--- module-3/src/app.ts
+++ module-3/src/app.ts
@@ -1,13 +1,32 @@
 import express, { Application, Request, Response } from "express";
+import fs from "fs";
+import path from "path";
 const app: Application = express();
+const filePath = path.join(__dirname, "../db/todo.json");
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  console.log(req.url, req.method);
+  res.send("Welcome to Todos App");
+});
+
+app.get("/todos", (req: Request, res: Response) => {
+  const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+  console.log(data);
+  res.json(data);
+});
+
+app.post("/todos/create-todo", (req: Request, res: Response) => {
+  const { title, body } = req.body;
+  console.log(title, body);
+  res.send("Creating todo");
 });
 
 export default app;
 
 /**
+ * Basic file structure
  * server - server handling like - starting, closing error handling of server. only related to server
  * app file - routing handle, middleware, route related error
  * app folder - app business logic handling like CRUD and database related work
