da1f37328328126a1fcccb60a95a0705441486c1
Raufur Islam <raufurislam@gmail.com>
2025-06-17 01:37:31 +0600

    Basic data type and require fields for scheema

--- advanced-note-app/src/app.ts
+++ advanced-note-app/src/app.ts
@@ -4,17 +4,32 @@ import { model, Schema } from "mongoose";
 const app: Application = express();
 
 const noteSchema = new Schema({
-  title: String,
-  content: String,
+  title: { type: String, require: true, trim: true },
+  content: { type: String, default: "" },
+  category: {
+    type: String,
+    enum: ["personal", "work", "study", "other"],
+    default: "personal",
+  },
+  pinned: {
+    type: Boolean,
+    default: false,
+  }, // type syntax in short we can use just Boolean but best is use full syntax
+  tags: {
+    label: { type: String, required: true },
+    color: { type: String, default: "gray" },
+  }, // object and type
 });
 
 const Note = model("Note", noteSchema);
 
 app.post("/create-note", async (req: Request, res: Response) => {
+  //   Approach - 1 of creating a data
   const myNote = new Note({
-    title: "Learning Mongoose",
-    content: true,
-    publishDate: "Hello...",
+    title: "Learning Mongodb",
+    tags: {
+      label: "database",
+    },
   });
 
   await myNote.save();
